82e1e2e6d84f9d4ed538933c9acd6e51d5ab996a
ramirez0801 <ramirez.erg@hotmail.com>
2022-08-24 21:49:37 -0500

    Solo falta la funcion eliminar, Version estable

--- main.cpp
+++ main.cpp
@@ -1,31 +1,74 @@
 //Emmanuel Ramirez Garcia
 //23 de agosto 2022
-//Datos 2
+//Seminario de Estructura de Datos 2
 
 #include <iostream>
+#include <conio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 struct album{
     int id;
     char nombre[25], artista[25], num_canc[3];
-}alb[50];
+}alb[20];
+
+void salto(){
+    cout<<"Presione enter para continuar..";
+    getch();
+    system("cls");
+}
 
 void agregar(int i){
-    
+    cout<<"Ingrese el nombre del album: ";
+    cin.getline(alb[i].nombre,25);
+    cout<<"Ingrese el numero de canciones del album: ";
+    cin.getline(alb[i].num_canc,5);
+    cout<<"Ingrese el nombre del artista: ";
+    cin.getline(alb[i].artista,25);
+    alb[i].id = i + 1;
+    salto();
 }
 
 void eliminar(){
 
 }
 
-void buscar(){
+void buscar(int c){
+    int bus;
+    cout<<"Ingrese el id a buscar: ";
+    cin>>bus;
 
+    for(int i = 0; i< c; i++){
+        if(alb[i].id == bus)
+        {
+            cout<<"ID: "<<alb[i].id<<endl;
+            cout<<"Nombre del album: "<<alb[i].nombre<<endl;
+            cout<<"Artista: "<<alb[i].artista<<endl;
+            cout<<"Numero de canciones: "<<alb[i].num_canc<<endl;
+            salto();
+            break;
+        }
+        else
+        {
+            cout<<"No encontrado";
+            break;
+        }
+            
+    }
 }
 
-void mostrar(){
-    
+void mostrar(int c){
+
+    for(int i = 0; i < c; i++){
+        cout<<"ID: "<<alb[i].id<<endl;
+        cout<<"Nombre del album: "<<alb[i].nombre<<endl;
+        cout<<"Artista: "<<alb[i].artista<<endl;
+        cout<<"Numero de canciones: "<<alb[i].num_canc<<endl;
+    }
+    salto();
 }
+
 
 
 
@@ -35,21 +78,17 @@ int main()
     
     
     int i = 0;
+    int c = 0;
     while(opc != 0){
-        cout<<"\n\t1) Ingresar \n\t2) Eliminar\n\t3) Buscar por id\n\t4) Mostrar\n Opc: ";
+        cout<<"\n\t1) Ingresar \n\t2) Eliminar\n\t3) Buscar por id\n\t4) Mostrar\n\t0 para salir \n Opc: ";
         cin>>opc;
         fflush(stdin);
         switch(opc){
             case 1:
             {   
-                cout<<"Ingrese el nombre del album: ";
-                cin.getline(alb[i].nombre,25);
-                cout<<"Ingrese el numero de canciones del album: ";
-                cin.getline(alb[i].num_canc,25);
-                cout<<"Ingrese el nombre del artista: ";
-                cin.getline(alb[i].nombre,25);
-                alb[i].id = i + 1;
+                agregar(i);
                 i++;
+                c++;
                 break;
             }
             case 2:
@@ -59,12 +98,12 @@ int main()
             }
             case 3:
             {
-                buscar();
+                buscar(c);
                 break;
             }
             case 4:
             {
-                mostrar();
+                mostrar(c);
                 break;
             }
             default:
