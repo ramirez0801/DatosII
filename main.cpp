be2c33d9bb51b78b9543521c4f85e12177a6c734
ramirez0801 <111304836+ramirez0801@users.noreply.github.com>
2022-08-25 10:48:29 -0500

    Revisar lista estatica

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 struct album{
     int id;
     char nombre[25], artista[25], num_canc[3];
-}alb[20];
+};
 
 void salto(){
     cout<<"Presione enter para continuar..";
@@ -19,7 +19,7 @@ void salto(){
     system("cls");
 }
 
-void agregar(int i){
+void agregar(int i, struct album alb[25]){
     cout<<"Ingrese el nombre del album: ";
     cin.getline(alb[i].nombre,25);
     cout<<"Ingrese el numero de canciones del album: ";
@@ -30,37 +30,60 @@ void agregar(int i){
     salto();
 }
 
-void eliminar(){
+int eliminar(int c, struct album alb[25]){
+	int bus;
+    cout<<"Ingrese el id a eliminar: ";
+    cin>>bus;
 
+    for(int i = 0; i<= c; i++)
+    {
+        if(alb[i].id == bus)
+        {
+        	alb[i] = {};
+        	cout<<"Registro eliminado";
+        	alb[i] = alb[i-1];
+        	break;
+        }
+        else
+        {
+			cout<<"No encontrado";
+			salto();   
+		}
+    }
+    return c;
+    
 }
 
-void buscar(int c){
+void buscar(int c, struct album alb[25]){
     int bus;
     cout<<"Ingrese el id a buscar: ";
     cin>>bus;
-
-    for(int i = 0; i< c; i++){
+	fflush(stdin);
+    for(int i = 0; i<= c; i++){
         if(alb[i].id == bus)
         {
             cout<<"ID: "<<alb[i].id<<endl;
             cout<<"Nombre del album: "<<alb[i].nombre<<endl;
             cout<<"Artista: "<<alb[i].artista<<endl;
             cout<<"Numero de canciones: "<<alb[i].num_canc<<endl;
+            cout<<"cc"<<c;
             salto();
             break;
         }
         else
         {
-            cout<<"No encontrado";
+            cout<<"No encontrado\n";
+            salto();
             break;
         }
             
     }
 }
 
-void mostrar(int c){
+void mostrar(int c, struct album alb[25]){
 
     for(int i = 0; i < c; i++){
+    	cout<<"\n";
         cout<<"ID: "<<alb[i].id<<endl;
         cout<<"Nombre del album: "<<alb[i].nombre<<endl;
         cout<<"Artista: "<<alb[i].artista<<endl;
@@ -74,11 +97,10 @@ void mostrar(int c){
 
 int main()
 {
-    int opc;
-    
-    
+    int opc = 1;
     int i = 0;
     int c = 0;
+    album alb[25];
     while(opc != 0){
         cout<<"\n\t1) Ingresar \n\t2) Eliminar\n\t3) Buscar por id\n\t4) Mostrar\n\t0 para salir \n Opc: ";
         cin>>opc;
@@ -86,24 +108,25 @@ int main()
         switch(opc){
             case 1:
             {   
-                agregar(i);
+                agregar(i, alb);
                 i++;
                 c++;
                 break;
             }
             case 2:
             {
-                eliminar();
+                c = eliminar(c, alb);
                 break;
             }
             case 3:
             {
-                buscar(c);
+                buscar(c, alb);
+                cout<<"CC: "<<c;
                 break;
             }
             case 4:
             {
-                mostrar(c);
+                mostrar(c, alb);
                 break;
             }
             default:
