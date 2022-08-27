7006dbb28a2bfdebf29d14bb3b28db8885339384
ramirez0801 <ramirez.erg@hotmail.com>
2022-08-26 21:27:58 -0500

    Complete, estable version

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ void salto(){
     system("cls");
 }
 
-void agregar(int i, struct album alb[25]){
+int agregar(int c, album alb[], int i){
     cout<<"Ingrese el nombre del album: ";
     cin.getline(alb[i].nombre,25);
     cout<<"Ingrese el numero de canciones del album: ";
@@ -27,60 +27,61 @@ void agregar(int i, struct album alb[25]){
     cout<<"Ingrese el nombre del artista: ";
     cin.getline(alb[i].artista,25);
     alb[i].id = i + 1;
+    c++;
+    return c;
     salto();
 }
 
-int eliminar(int c, struct album alb[25]){
-	int bus;
+int eliminar(int c, album alb[]){
+	int bus,b =0 ;
     cout<<"Ingrese el id a eliminar: ";
     cin>>bus;
 
-    for(int i = 0; i<= c; i++)
+    for(int i = 0; i <= c; i++)
     {
         if(alb[i].id == bus)
         {
-        	alb[i] = {};
+            for(int j = i; j < c; j++)
+        	    alb[j] = alb[j+1];
         	cout<<"Registro eliminado";
-        	alb[i] = alb[i-1];
+            c--;
+            b = 1;
         	break;
         }
-        else
-        {
-			cout<<"No encontrado";
-			salto();   
-		}
+    }
+    if(b == 0)
+    {
+        cout<<"No se encontro";
+        salto();
     }
     return c;
     
 }
 
-void buscar(int c, struct album alb[25]){
-    int bus;
+void buscar(int c, album alb[]){
+    int bus, encontrado = 0;
     cout<<"Ingrese el id a buscar: ";
     cin>>bus;
-	fflush(stdin);
-    for(int i = 0; i<= c; i++){
+    for(int i = 0; i < c; i++){
         if(alb[i].id == bus)
         {
             cout<<"ID: "<<alb[i].id<<endl;
             cout<<"Nombre del album: "<<alb[i].nombre<<endl;
             cout<<"Artista: "<<alb[i].artista<<endl;
             cout<<"Numero de canciones: "<<alb[i].num_canc<<endl;
-            cout<<"cc"<<c;
             salto();
+            encontrado = 1;
             break;
-        }
-        else
-        {
-            cout<<"No encontrado\n";
-            salto();
-            break;
-        }
-            
+        }        
     }
+    if(encontrado == 0){
+        cout<<"No se encontro";
+        salto();
+    }
+    
 }
 
-void mostrar(int c, struct album alb[25]){
+void mostrar(int c, album alb[]){
 
     for(int i = 0; i < c; i++){
     	cout<<"\n";
@@ -88,6 +89,7 @@ void mostrar(int c, struct album alb[25]){
         cout<<"Nombre del album: "<<alb[i].nombre<<endl;
         cout<<"Artista: "<<alb[i].artista<<endl;
         cout<<"Numero de canciones: "<<alb[i].num_canc<<endl;
+        cout<<"\n";
     }
     salto();
 }
@@ -98,9 +100,10 @@ void mostrar(int c, struct album alb[25]){
 int main()
 {
     int opc = 1;
-    int i = 0;
-    int c = 0;
-    album alb[25];
+    int c = 0, i = 0;
+
+    album alb[4];
+
     while(opc != 0){
         cout<<"\n\t1) Ingresar \n\t2) Eliminar\n\t3) Buscar por id\n\t4) Mostrar\n\t0 para salir \n Opc: ";
         cin>>opc;
@@ -108,9 +111,8 @@ int main()
         switch(opc){
             case 1:
             {   
-                agregar(i, alb);
+                c = agregar(c, alb, i);
                 i++;
-                c++;
                 break;
             }
             case 2:
@@ -121,7 +123,6 @@ int main()
             case 3:
             {
                 buscar(c, alb);
-                cout<<"CC: "<<c;
                 break;
             }
             case 4:
@@ -132,10 +133,13 @@ int main()
             default:
             {
                 if(opc == 0)
+                {
                     cout<<"Saliendo...";
+                    break;
+                }
                 else
                     cout<<"Opcion no valida";
-                break;
+                
             }
         }
 
